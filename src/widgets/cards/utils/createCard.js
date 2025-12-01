bb45f389652c0868a0ca3dd17958b5b6dcf105d7
pross <caiiiia_podoplelov@mail.ru>
2025-12-01 18:00:29 +0300

    finish card styles

--- src/widgets/cards/utils/createCard.js
+++ src/widgets/cards/utils/createCard.js
@@ -1,43 +1,50 @@
-import { cardsBlockSelector } from "..//consts/selectors";
-import { getCardBagColor } from "./getCardBagColor";
+import { cardsBlockSelector } from '..//consts/selectors';
+import { getCardBagColor } from './getCardBagColor';
 
 export const createCard = (card) => {
   const { img, name, bag, price, author } = card;
 
   const cardsBlock = document.querySelector(cardsBlockSelector);
 
-  const cardBlock = document.createElement("div");
-  cardBlock.className = "cards__elem";
+  const cardBlock = document.createElement('div');
+  cardBlock.className = 'cards__elem';
   cardsBlock.appendChild(cardBlock);
 
-  const cardImg = document.createElement("img");
+  const cardImg = document.createElement('img');
   cardImg.src = img;
   cardImg.alt = name;
   cardBlock.appendChild(cardImg);
 
-  const infoBlock = document.createElement("div");
-  infoBlock.className = "cards__info";
+  const infoBlock = document.createElement('div');
+  infoBlock.className = 'cards__info';
   cardBlock.appendChild(infoBlock);
 
-  const bagElem = document.createElement("span");
-  bagElem.className = "cards__bag";
+  const bagElem = document.createElement('span');
+  bagElem.className = 'cards__bag';
   bagElem.classList.add(getCardBagColor(bag));
   bagElem.textContent = bag;
-
-  const nameElem = document.createElement("p");
-  nameElem.textContent = name;
   infoBlock.appendChild(bagElem);
+
+  const nameElem = document.createElement('p');
+  nameElem.textContent = name;
+  nameElem.className = 'cards__name';
   infoBlock.appendChild(nameElem);
 
-  const postInfoBlock = document.createElement("div");
-  postInfoBlock.className = "cards__postinfo";
+  const postInfoBlock = document.createElement('div');
+  postInfoBlock.className = 'cards__postinfo';
   infoBlock.appendChild(postInfoBlock);
 
-  const priceElem = document.createElement("span");
+  const priceElem = document.createElement('span');
   priceElem.textContent = price;
-
-  const authorElem = document.createElement("p");
-  authorElem.textContent = author;
+  priceElem.className = 'cards__price';
   postInfoBlock.appendChild(priceElem);
+
+  const sep = document.createElement('span');
+  sep.className = 'cards__sep';
+  postInfoBlock.appendChild(sep);
+
+  const authorElem = document.createElement('p');
+  authorElem.textContent = author;
+  authorElem.className = 'cards__author';
   postInfoBlock.appendChild(authorElem);
 };
